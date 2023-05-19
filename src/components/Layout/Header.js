21d50382caed8f0e95527185e7c317a4f9dd3c54
gabedavaa <110985736+gabedavaa@users.noreply.github.com>
2023-05-19 21:54:00 +0400

    adding cart-context and cart provider

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -2,12 +2,13 @@ import React, { Fragment } from "react";
 import HeaderCartButton from "./HeaderCartButton";
 import mealsImage from "../../assets/meals.jpg";
 import classes from "./Header.module.css";
+
 function Header(props) {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>ReactMeals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of deliciuos food!" />
